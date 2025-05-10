ce0825ef7579eca78fdec79b799c7e565a7fc975
Onica Lorena <onica.lorena05@gmail.com>
2025-05-10 23:32:41 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import engine, Base
 from app.routers import users
 from app.routers import conversations
 from app.routers import messages
+from app.routers import vector_response
+
 app = FastAPI()
 
-# Creează automat toate tabelele dacă nu există
+# Adăugăm CORS ca să putem comunica cu frontend-ul
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Creează automat toate tabelele
 @app.on_event("startup")
 def on_startup():
     print("Verific tabelele în baza de date...")
@@ -16,3 +32,4 @@ def on_startup():
 app.include_router(users.router)
 app.include_router(conversations.router)
 app.include_router(messages.router)
+app.include_router(vector_response.router)
