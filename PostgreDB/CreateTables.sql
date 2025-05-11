d976636761222f6691dea3e55c2c06c4adf87372
Onica Lorena <onica.lorena05@gmail.com>
2025-05-11 15:04:51 +0300

    Update CreateTables.sql

--- PostgreDB/CreateTables.sql
+++ PostgreDB/CreateTables.sql
@@ -6,23 +6,26 @@ CREATE TABLE users (
     auth0_id VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE,
+    phone VARCHAR(255),
+    gender VARCHAR(50),
+    language VARCHAR(50),
+    country VARCHAR(50),
+    company VARCHAR(255) DEFAULT 'DOSETIMPEX',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE conversations (
     id UUID PRIMARY KEY,
-    user_id UUID,
+    user_id UUID REFERENCES users(id),
     title VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE messages (
     id UUID PRIMARY KEY,
-    conversation_id UUID,
+    conversation_id UUID REFERENCES conversations(id),
     sender TEXT NOT NULL,
     content TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (conversation_id) REFERENCES conversations(id)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
