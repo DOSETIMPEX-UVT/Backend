558a83c3b47648f697ecaf350c6093c5f6df0e56
onica-lorena <onica.lorena05@gmail.com>
2025-05-17 21:30:57 +0300

    add document button

--- app/routers/messages.py
+++ app/routers/messages.py
@@ -1,8 +1,14 @@
 from typing import List
 from uuid import UUID
-
+from fastapi import UploadFile, File
+import os, tempfile, sys
+from subprocess import run
+from fastapi.responses import JSONResponse
+from typing import Optional
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from app.services.messages import add_message
+from uuid import UUID
 
 from app.auth import get_current_user  # funcția care validează tokenul și extrage userul
 from app.database import get_db
@@ -27,6 +33,39 @@ async def add_message(
     response_from_LLM = await generate_response_from_LLM(message_model.sender)
     message = add_message(db, message_model.conversation_id, message_model.sender, response_from_LLM)
     return message
+
+# Adaugare document .docx
+@router.post("/upload-file")
+async def upload_file(file: UploadFile = File(...)):
+    suffix = os.path.splitext(file.filename)[-1].lower()
+    if suffix != ".docx":
+        return JSONResponse(content={"error": "Se acceptă doar fișiere .docx."}, status_code=400)
+
+    try:
+        raw_name = file.filename.strip().lower().replace(" ", "_")
+        os.makedirs("storage/temp_files", exist_ok=True)
+        raw_path = os.path.join("storage/temp_files", raw_name)
+
+        with open(raw_path, "wb") as f:
+            f.write(await file.read())
+
+        run([sys.executable, "upload_processing/process_uploaded_file.py", raw_name])
+
+        processed_path = os.path.join("storage/processed", raw_name.replace(suffix, ".txt"))
+        if not os.path.exists(processed_path):
+            return JSONResponse(content={"error": "Textul nu a fost extras corect."}, status_code=400)
+
+        with open(processed_path, "r", encoding="utf-8") as f:
+            content = f.read()
+
+        return {"extractedText": content[:2000]}
+
+    except Exception as e:
+        import traceback
+        print("Eroare la upload:", traceback.format_exc())
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+
+
 
 @router.get("/all_conversation/{conversation_id}", response_model=List[SenderContentDto])
 def all_conversation(
